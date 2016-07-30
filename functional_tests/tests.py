7b5bff80190bb390bc05bc9df7dc98667871253f
Kirk DeBaets <kirk_debaets@bmc.com>
2016-07-30 14:20:50 -0400

    new URL, view and template to display lists

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -57,7 +57,7 @@ class NewVisitorTest(LiveServerTestCase):
         ## We use a new browser session to make sure none of Sara's info
         ## is coming in via cookies.
         self.browser.quit()
-        self. browser.Firefox()
+        self.browser = webdriver.Firefox()
 
         # Francis visits the home page.  There is no sign of Sara's list
         self. browser.get(self.live_server_url)
