0919a307fe4d3f0184886641298e832b2d3f09cf
Kirk DeBaets <kirk_debaets@bmc.com>
2016-08-11 15:58:45 -0400

    Fabric file corrected again

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -43,7 +43,7 @@ def _update_settings(source_folder, site_name):
             'ALLOWED_HOSTS = ["%s"]' % (site_name,)
     )
     secret_key_file = source_folder + '/superlists/secret_key.py'
-    if not exists(secre_key_file):
+    if not exists(secret_key_file):
         chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
         key = ''.join(random.SystemRandom().choice(chars) for _ in range(50))
         append(secret_key_file, "SECRET_KEY = '%s'" % (key,))
