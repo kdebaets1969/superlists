1dfc3ec2a9fc9772c093bfdaa67975ab02bbcd94
Kirk DeBaets <kirk_debaets@bmc.com>
2016-08-18 22:37:07 -0400

    rename all item input ids and names.  still broken

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -5,7 +5,7 @@ class ItemValidationTest(FunctionalTest):
            # Sara goes to the home page and accidentally tries to submit
            # an empty list item
            self.browser.get(self.server_url)
-           self.browser.find_element_by_id('id_new_item').send_keys('\n')
+           self.get_item_input_box().send_keys('\n')
 
            # The home page refreshes and there is an error message saying that
            # list items cannot be blank.
@@ -13,11 +13,11 @@ class ItemValidationTest(FunctionalTest):
            self.assertEqual(error.text, "You can't have an empty list item")
 
            # She tries again with characters and it works.
-           self.browser.find_element_by_id('id_new_item').send_keys('Buy milk\n')
+           self.get_item_input_box().send_keys('Buy milk\n')
            self.check_for_row_in_list_table('1: Buy milk')
            
            # To mess with me, she tries to submit a blank item again.
-           self.browser.find_element_by_id('id_new_item').send_keys('\n')
+           self.get_item_input_box().send_keys('\n')
 
            # She receives a similar warning.
            self.check_for_row_in_list_table('1: Buy milk')
@@ -25,6 +25,6 @@ class ItemValidationTest(FunctionalTest):
            self.assertEqual(error.text, "You can't have an empty list item")
 
            # And she can correct by filling in some text
-           self.browser.find_element_by_id('id_new_item').send_keys('Make tea\n')
+           self.get_item_input_box().send_keys('Make tea\n')
            self.check_for_row_in_list_table('1: Buy milk')
            self.check_for_row_in_list_table('2: Make tea')
--- functional_tests/test_simple_list_creation.py
+++ functional_tests/test_simple_list_creation.py
@@ -15,7 +15,7 @@ class NewVisitorTest(FunctionalTest):
         self.assertIn('To-Do', header_text)
 
         # She is invited to enter a to-do item straight away
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.get_item_input_box()
         self.assertEqual(inputbox.get_attribute('placeholder'), 
                 'Enter a to-do item')
 
@@ -32,7 +32,7 @@ class NewVisitorTest(FunctionalTest):
 
         # there is still a text box inviting her to add another item.  
         # She enters "use peacock feathers to make a fly"
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.get_item_input_box()
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
 
@@ -54,7 +54,7 @@ class NewVisitorTest(FunctionalTest):
 
         # Francis starts a new list by entering a new item.  
         # He is less interesting than Sara
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.get_item_input_box()
         inputbox.send_keys('Buy milk')
         inputbox.send_keys(Keys.ENTER)
 
